973d1273bb14cc5a52ebe4b77977966d4d60f130
JokerMrx <renat.lakyziuk@gmail.com>
2024-07-06 23:10:30 +0300

    fixing a bug with adaptability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container !px-4 md:px-6 lg:px-8">
       <div className="card hero">
-        <p className="text-display-1 hero-title">
+        <p className="mb-5 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
           {"Let's"} take a look at <br /> Cryptocurrencies
         </p>
 
